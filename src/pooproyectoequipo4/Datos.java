70259e23c4f101e1023f2f562183cc247d8de7c2
Envy <Envy@DESKTOP-7I42Q68>
2021-05-07 17:07:20 -0600

    Inicio de proyecto

---
+++ src/pooproyectoequipo4/Datos.java
@@ -0,0 +1,26 @@
+
+package pooproyectoequipo4;
+
+import javax.swing.JOptionPane;
+
+
+public class Datos {
+    //Se declaran atributos
+    protected String NombreEmpresa;
+    protected double ingresosdiarios, egresosdiarios;
+    
+    public Datos() {
+        this.NombreEmpresa = NombreEmpresa;
+        this.ingresosdiarios = ingresosdiarios;
+        this.egresosdiarios = egresosdiarios;
+    }
+    
+    public void PedirDatos () {
+        NombreEmpresa = JOptionPane.showInputDialog("Ingrese el Nombre de su empresa: ");
+        ingresosdiarios = Double.parseDouble(JOptionPane.showInputDialog("¿Cuántos son los 'ingresos' diarios en su empresa? "));
+        egresosdiarios = Double.parseDouble(JOptionPane.showInputDialog("¿Cuántos son los 'egresos' diarios en su empresa? "));
+        System.out.println(NombreEmpresa + ingresosdiarios + egresosdiarios); //Salida de consola para comprobar si funciona bien el codigo
+        
+    }
+    
+}
---
+++ src/pooproyectoequipo4/POOProyectoEquipo4.java
@@ -0,0 +1,28 @@
+/*
+ * 
+ */
+package pooproyectoequipo4;
+
+/**
+* @Equipo 4, POO
+* Bryan Aaron Sigala Jaime
+* Ana Cristina Valenzuela Ruiz
+* Dianelly Amaya Vega
+* Edwin Santos Marquez
+* Tania Jaqueline Ramirez Ramos
+* Marco Antonio Ortega Sarmiento
+* Brandon Isaac Leyva Gonzalez
+*/
+public class POOProyectoEquipo4 {
+
+    
+    public static void main(String[] args) {
+        
+        //Aqui mandamos a llamar todo
+        Datos datos = new Datos();
+        
+        datos.PedirDatos();
+        
+    }
+    
+}
