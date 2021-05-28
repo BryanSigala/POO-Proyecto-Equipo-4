b045090e71b0a2015edc1e278de4322028ac727e
anacr <anacr@DESKTOP-K9QUO6S>
2021-05-28 12:47:33 -0600

    Figuras...

--- src/mis_figuras/Pruebas.java
+++ src/mis_figuras/Pruebas.java
@@ -15,6 +15,8 @@ public class Pruebas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //Figuras Hola
         Rectangulo r1 = new Rectangulo(2.0, 4.0);
         System.out.println("Rectangulo");
 
